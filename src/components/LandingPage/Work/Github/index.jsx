856b4b9f44140c461804b85a957cb4ec96652141
Misa <dieuhuongngo@gmail.com>
2019-06-03 00:09:00 +0300

    more projects

--- src/components/LandingPage/Work/Github/index.jsx
+++ src/components/LandingPage/Work/Github/index.jsx
@@ -11,39 +11,39 @@ const Github = () => {
   const repositories = [
     {
       id: 0,
-      title: 'Hacker News',
-      image: `${HackerNews}`,
-      link: 'https://hacker-news-search.netlify.com/'
-    },
-    {
-      id: 1,
-      title: 'Youtube Video Search',
-      image: `${Youtube}`,
-      link: 'https://youtube-video-search.netlify.com/'
-    },
-    {
-      id: 2,
-      title: 'Burger Order',
-      image: `${Burger}`,
-      link: 'https://burger-order.netlify.com/'
-    },
-    {
-      id: 3,
-      title: 'Personal Trainer Database',
-      image: `${PersonalTrainer}`,
-      link: 'https://personal-trainer-database.netlify.com/'
-    },
-    {
-      id: 4,
       title: 'Telia English Menu',
       image: `${TeliaMenu}`,
       link: 'https://telia-english-menu.netlify.com/'
     },
     {
-      id: 5,
+      id: 1,
       title: 'UpCloud Servers',
       image: `${Upcloud}`,
       link: 'https://misa-upcloud.netlify.com/'
+    },
+    {
+      id: 2,
+      title: 'Hacker News',
+      image: `${HackerNews}`,
+      link: 'https://hacker-news-search.netlify.com/'
+    },
+    {
+      id: 3,
+      title: 'Youtube Video Search',
+      image: `${Youtube}`,
+      link: 'https://youtube-video-search.netlify.com/'
+    },
+    {
+      id: 4,
+      title: 'Burger Order',
+      image: `${Burger}`,
+      link: 'https://burger-order.netlify.com/'
+    },
+    {
+      id: 5,
+      title: 'Personal Trainer Database',
+      image: `${PersonalTrainer}`,
+      link: 'https://personal-trainer-database.netlify.com/'
     }
   ]
   return (
